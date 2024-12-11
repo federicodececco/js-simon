35ee0c9eaa6867f120ccb90ea993b759ca19aa2b
federicodececco <federicodececco1@gmail.com>
2024-12-11 15:37:36 +0100

    add; html structure

---
+++ script.js
@@ -0,0 +1,26 @@
+const randomEasy = () => {
+    return Math.floor(Math.random() * (99) + 0)
+}
+
+document.getElementById("dFirst").innerHTML = randomEasy();
+document.getElementById("dSecond").innerHTML = randomEasy();
+document.getElementById("dThird").innerHTML = randomEasy();
+document.getElementById("dFourth").innerHTML = randomEasy();
+document.getElementById("dFifth").innerHTML = randomEasy();
+const first = document.getElementById("first")
+const second = document.getElementById("second")
+const third = document.getElementById("third")
+const fourth = document.getElementById("fourth")
+const fifth = document.getElementById("fifth")
+
+
+
+
+
+
+
+
+const send = getElementById("go");
+send.addEventListener("click", function () {
+    document.getElementById("display")
+})
